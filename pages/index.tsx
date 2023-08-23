b3deeed0a669b47bb26d5fdb40e965b321cd489a
Harsh Keshari <harshkeshari@Harshs-MacBook-Air.local>
2023-08-23 22:51:18 +0530

    finishing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,9 @@
 import { useSession } from "next-auth/react";
 import type { NextPage } from "next";
 import HomePage from "@/templates/HomePage";
-import Link from "next/link";
 
 const Home: NextPage = () => {
-  const { data: session, status } = useSession();
-
-  if (status === "authenticated") {
-    return <HomePage />;
-  }
-  return <Link href="/api/auth/signin">Sign in</Link>;
+  return <HomePage />;
 };
 
 export default Home;
